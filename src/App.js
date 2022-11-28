4fe8c9b5b69bb68de2ecf05a026f2bd148fa5401
Kairo Ribeiro <kairo10@gmail.com>
2022-11-28 13:21:45 -0500

    add search

--- src/App.js
+++ src/App.js
@@ -7,22 +7,53 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch(FEATURED_API)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setMovies(data.results);
       });
   }, []);
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+    fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+      setSearchTerm('')
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
   return (
-    <div className='movie-container'>
-      {movies.length > 0 && movies.map((movie) => 
-        <Movie key={movie.id} {...movie}/>
-      )}
-    </div>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className='search'
+            type='text'
+            placeholder='Search...'
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className='movie-container'>
+
+        {movies.length > 0 && movies.map((movie) =>
+          <Movie key={movie.id} {...movie} />
+        )}
+      </div>
+    </>
   );
 }
 
