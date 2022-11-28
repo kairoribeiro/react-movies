7059f0c6fcc6cbd6cde441352fec054ca6881107
Kairo Ribeiro <kairo10@gmail.com>
2022-11-28 11:37:30 -0500

    add Movie comp

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Movie from './components/Movie';
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function App() {
@@ -19,9 +18,9 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className='movie-container'>
       {movies.length > 0 && movies.map((movie) => 
-        <Movie />
+        <Movie key={movie.id} {...movie}/>
       )}
     </div>
   );
